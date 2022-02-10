e9f007411988905500a9f8acd67b652e7a62bc4d
sonnnnnnnny <98822790+sonnnnnnnny@users.noreply.github.com>
2022-02-10 10:51:35 +0700

    stress

--- app/js/script.js
+++ app/js/script.js
@@ -1,5 +1,25 @@
-console.log('HELLO');
+const numberpattern = /^[0-9/*--+^xy]*$/;
+let equation = 1;
+let equation = document.querySelector('#display'+ equation);
+let currentequation = equation.dataset.number;
 
-const test = () => {
-	console.log('this is a test');
+document.addEventListener('keydown',(e) => {
+	console.log('keypress :'+ e.key);
+let keypress = e.key;
+if (numberpattern.length == 1 && numberpattern.test(e.key)){
+	let isnumber = numberpattern.test(e.key);
+    console.log(isnumber);
+    updatedisplay(keypress);
+
+}
+});
+const updatedisplay = (number) =>  {
+	console.log('display =' + equation
+	/ncurrentequation
+	);
+
 };
+let equation = ('equation ='+ equation); 
+const updatingdisplay = () => {
+
+}
