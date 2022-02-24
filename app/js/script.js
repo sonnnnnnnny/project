f11b9174945d0ff4cda91fc2cb7aabc0a98f7341
sonnnnnnnny <98822790+sonnnnnnnny@users.noreply.github.com>
2022-02-24 10:31:49 +0700

    i dont know

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,9 @@
+<<<<<<< HEAD
 const { get } = require("browser-sync")
 const algebrite = require('algebrite')
 
+=======
+>>>>>>> parent of 13706ac (fail)
 //function that display value
 function dis(val)
 {
@@ -20,5 +23,14 @@ function clr()
 {
     document.getElementById("result").value = ""
 }
+<<<<<<< HEAD
 
+=======
+//exponential symbol &sup2;
+>>>>>>> parent of 13706ac (fail)
 
+//sqrt <&radic;>
+//x  
+let x = Symbol(("x"));
+//y
+let y = Symbol(("y"));
